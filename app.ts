79c077123ded038fcd5ede234430741d1dfbd190
Madeel1234 <164627451+Madeel1234@users.noreply.github.com>
2024-04-14 15:10:56 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+let places: string[] = ["Capetown","Delhi","Faisalabad","Bankok","Ahmadabad"];
+console.log('original' + places);
+// Print your array in albhabetical order
+console.log('copy' +[...places].sort());
+// Show that your array in its original order by printing in it.
+console.log('original' + places);
+// Print reverse alphabetical
+console.log('copy' +[...places].sort().reverse());
+// show that your array still in its original order by printing it agagin.
+console.log('copy' +[...places].sort().reverse());
+//Reversre the order of your list. Print the array to show that its order has been changed
+console.log('original' + places.sort());
+//Reversre the order of your list again
+console.log('original' + places.sort().reverse());
+//Sort your array so its stored in alphabetical order
+console.log('copy' + [...places].sort());
